f436dcd6806e8d68d5ee80be701797943bbf6ec1
JeffJohnson <jeff.jj.9494@gmail.com>
2019-04-16 20:04:34 -0600

    reducers are done and axios is being used working on component tree right now

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import{FETCHING_CHARS, FETCHING_CHARS_SUCCESS, FETCHING_CHARS_FAILURE} from "../actions";
 const initialState = {
   characters: []
+  fetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,12 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING_CHARS:
+      return{...state,fetching:true};
+    case FETCHING_CHARS_SUCCESS:
+      return{...state,fetching:false,characters:[...state.characters,...action.payload]};
+    case FETCHING_CHARS_FAILURE:
+      return{...state,fetching:false,error:action.payload};
     default:
       return state;
   }
