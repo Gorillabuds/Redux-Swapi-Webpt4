9c7bc72458bb2abf3bf3834eae4dedc85ea5b7c5
JeffJohnson <jeff.jj.9494@gmail.com>
2019-07-16 17:59:48 -0600

    working on getting planets and species to render

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 export const FETCHING_CHARS='FETCHING_CHARS';
 export const FETCHING_CHARS_SUCCESS='FETCHING_CHARS_SUCCESS';
 export const FETCHING_CHARS_FAILURE='FETCHING_CHARS_FAILURE';
+export const FETCHING_HOME='FETCHING_HOME';
+export const FETCHING_HOME_SUCCESS='FETCHING_HOME_SUCCESS';
+export const FETCHING_HOME_FAILURE='FETCHING_HOME_FAILURE';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
@@ -19,5 +22,16 @@ export const fetchChars=()=>dispatch=>{
         })
         .catch(err=>{
             dispatch({type:FETCHING_CHARS_FAILURE, payload: err});
+        })
+}
+export const fetchHome=()=>dispatch=>{
+    dispatch({type:FETCHING_HOME});
+    axios.get('https://swapi.co/api/planets/')
+        .then(response=>{
+            console.log('res',response)
+            dispatch({type:FETCHING_HOME_SUCCESS,payload:response.data.results});
+        })
+        .catch(err=>{
+            dispatch({type:FETCHING_HOME_FAILURE, payload:err});
         })
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Character = props => {
-  return <li>{props.character.name}</li>;
+  return <li>{props.character.name} 
+              <p>{props.character.homeworld}</p>
+              <p>{props.character.birth_year}</p>
+              <p>{props.character.species}</p>
+        </li>;
 };
 
 export default Character;
